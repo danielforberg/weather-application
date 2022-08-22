027ea016931e248feb6717866e60ea6b95aa9aaa
Daniel Förberg <daniel.forberg@storytelconsultant.com>
2022-08-22 08:50:42 +0200

    Adjust code

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from os import getenv
 
 DEFAULT_LOCATION = "Stockholm"
 DEFAULT_BASE_URL = "https://api.openweathermap.org/data/3.0/onecall"
-DEFAULT_API_KEY = "dc1c0c06a6c8f72a0c16e886cb9ceb53"
+DEFAULT_API_KEY = "c53f51022b3280736475e4a0b4ced320"
 
 
-def forecast(place=None):
+def forecast_http_request(request):
+    return forecast(request)
+
+
+def forecast(request):
+    place = request.args.get('place')
     locator = Nominatim(user_agent="weatherForcast")
     if not place:
         location = locator.geocode(DEFAULT_LOCATION)
@@ -16,7 +21,7 @@ def forecast(place=None):
         location = locator.geocode(place)
 
     base_url = getenv('BASE_URL', DEFAULT_BASE_URL)
-    api_key = getenv('API_KEY', DEFAULT_BASE_URL)
+    api_key = getenv('API_KEY', DEFAULT_API_KEY)
     lat = location.latitude
     lon = location.longitude
     excludes = "hourly,daily,minutely"
@@ -30,4 +35,5 @@ def forecast(place=None):
 
 
 if __name__ == '__main__':
+
     print(forecast("Stockholm"))
